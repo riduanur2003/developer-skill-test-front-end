f236303d23906cd2f5a7ee1657bebd9e7d01cc5b
riduanur2003 <riduanur2003@gmail.com>
2021-06-28 14:42:53 +0100

    changes to css
    most of the works are done with the css. Some more fine tuning can be done. But they are really minor in my opinon. Didnt render description in the cards as 
    some objects has description and some does not. Just adding {pic.description} should do it. 
    
    some more features could have been added. its just the time frame ......... Not on your part. I would think 48 hours is enough. But I got a AWS devops exam which I am getting ready for and already booked the exam. So just under tremendous pressure.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
   componentDidMount() {
     fetch(
-      "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=8ab9ce661f671bb8d7e7e29b283f7594&tags=cats&extras=description&per_page=12&page=2&format=json&nojsoncallback=1"
+      "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=8ab9ce661f671bb8d7e7e29b283f7594&tags=nyc&extras=description&per_page=12&page=2&format=json&nojsoncallback=1"
     )
       .then(function (response) {
         return response.json();
@@ -27,13 +27,26 @@ class App extends Component {
               pic.id +
               "_" +
               pic.secret +
-              "_q.jpg";
+              ".jpg";
             return (
               <div className="App-intro">
-                <a href={srcPath}>
-                  <img alt="dogs" src={srcPath}></img>
-                  <h4>{pic.title}</h4>
-                </a>
+                <div className="card-containter">
+                  <a href={srcPath}>
+                    <img className="image" alt="dogs" src={srcPath}></img>
+                  </a>
+                  <div className="card-header"> {pic.title}</div>
+
+                  <div className="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque elementum ultrices pellentesque. Aliquam congue
+                    metus.
+                  </div>
+                  <div className="card-footer">
+                    <button className="buttonStyle" type="button ">
+                      Explore
+                    </button>
+                  </div>
+                </div>
               </div>
             );
           });
